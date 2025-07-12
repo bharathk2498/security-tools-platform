dda57d9d658f84c3fcf80dd2d28e68602fb5753f
BHARATH KUMAR BYRU <bharathk9339@gmail.com>
2025-07-12 15:23:21 -0400

    Add NIST compliance module __init__.py

---
+++ nist_compliance/src/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+NIST 800-53 Compliance Framework Module
+Policy-as-code implementation with automated monitoring and remediation
+"""
+
+from .nist_compliance import (
+    ComplianceOrchestrator,
+    NISTControl,
+    ComplianceAssessment,
+    TerraformGenerator,
+    OPAPolicyGenerator,
+    NISTControlDatabase,
+    InfrastructureScanner,
+    ComplianceStatus,
+    ControlFamily
+)
+
+__version__ = "1.0.0"
+__author__ = "Bharath Kumar"
+
+__all__ = [
+    "ComplianceOrchestrator",
+    "NISTControl",
+    "ComplianceAssessment",
+    "TerraformGenerator",
+    "OPAPolicyGenerator", 
+    "NISTControlDatabase",
+    "InfrastructureScanner",
+    "ComplianceStatus",
+    "ControlFamily"
+]
